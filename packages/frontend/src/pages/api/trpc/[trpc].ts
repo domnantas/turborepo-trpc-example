6b36a479a735f51f725f5bf25883e30dbcca3930
domnantas <hello@domnantas.lt>
2023-04-07 14:46:40 +0300

    Move tRPC api route to correct directory

--- packages/frontend/src/pages/api/trpc/[trpc].ts
+++ packages/frontend/src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createNextApiHandler } from "@trpc/server/adapters/next";
-import { appRouter } from "backend/";
+import { appRouter, createTRPCContext } from "backend";
 
 export default createNextApiHandler({
   router: appRouter,
